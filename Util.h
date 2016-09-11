555c02516ee075156abe7b37efa53d00985bf974
HexPang <hexpang@jabstruse.com>
2016-09-12 00:58:54 +0800

    Data package upload

--- Util.h
+++ Util.h
@@ -16,4 +16,7 @@ typedef void (^UploadProgress)(int code,float progress,NSObject *message);
 - (NSURL *) getCacheFolder:(NSString *)name;
 - (LxFTPRequest *) UploadWithFTP:(NSString *)localFile withName:(NSString *)name withProgress:(UploadProgress) uProgress;
 - (LxFTPRequest *) ListWithFTP:(NSString *)path withProgress:(UploadProgress) uProgress;
+
+- (BOOL) UploadFolderWithFTP:(NSString *)folder toPath:(NSString *)toPath withProgress:(UploadProgress)progress;
+- (void) CreateFolderBeforeUpload:(NSString *)folder toPath:(NSString *)toPath withProgress:(UploadProgress)progress;
 @end
